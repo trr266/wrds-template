914b9e182e63d9ff7da02e970f74dd72e5c674bb
timnahw <156186100+timnahw@users.noreply.github.com>
2024-03-14 15:50:29 +0100

    Add duckdb library and integrate with WRDS data***

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 import wrds
-db = wrds.Connection()
-#db.create_pgpass_file()
+import pandas as pd
+import duckdb
+import os
 
-libraries = db.list_libraries()
-libraries[:4]
 
-tables = db.list_tables('wrdssec')
-tables[:4]
+table_name = 'bank_fundq'
 
-col_headers = db.describe_table(library="wrdssec", table="wrds_nlpsa")
+# Fetch data from WRDS
+db = wrds.Connection(wrds_username=os.environ.get('WRDS_USERNAME'))
+data = db.get_table(library='comp', table = table_name, obs=10)
+
+# Load data into a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Initialize a DuckDB connection
+duckdb_conn = duckdb.connect()
+
+# Load the DataFrame into a DuckDB table
+duckdb_conn.from_df(df, table_name)
+
+# Save the DuckDB table to a Parquet file
+duckdb_conn.execute(f"COPY {table_name} TO '{table_name}.parquet' (FORMAT 'PARQUET')")
